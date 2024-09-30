c84b3589247d2f83f8cc91a6cd3062ce22b9623b
Alena Prochazkova <alena.prochazkova@gmail.com>
2024-09-30 21:07:16 +0300

    hod kostkou

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const studenti = navstevnici * 0.4
 const cenaListkuStudent = cenaListku * 0.65
 const prijemDivadlaSestudenty = (studenti * cenaListkuStudent * pocetPredstaveniMesicne) + ((navstevnici - studenti) * cenaListku *pocetPredstaveniMesicne)
 document.body.innerHTML += "<p> Prijem divadla se studenty za mesic: " + prijemDivadlaSestudenty + " Eur</p>" 
+
+// Hod kostkou
+document.body.innerHTML += "<h4>--- Priklad: Hazeni kostkou - Math random ----------------------------------- " + "</h4>"
+
+const hodKostkou = Math.floor(Math.random()*6)+1;
+document.body.innerHTML += "<p> Hod kostkou: " + hodKostkou + "</p>" 
